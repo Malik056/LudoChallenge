95517f2e392ff84e167cf63f350576f67b165d12
LudoChallenge <antelopes06@gmail.com>
2018-04-29 14:56:52 +0500

    waiting for another player

--- app/src/main/java/com/example/apple/ludochallenge/networking/SettingsActivity.java
+++ app/src/main/java/com/example/apple/ludochallenge/networking/SettingsActivity.java
@@ -95,6 +95,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        //
 
 
         ActivityCompat.requestPermissions(
